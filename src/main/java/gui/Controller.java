ea25b660998daf4dacd1cd174bd0a3524fd5f8ac
Timdpr <QOTimSA@live.co.uk>
2018-04-16 17:06:43 +0100

    Add initial drag&drop functionality

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,5 +1,8 @@
 package main.java.gui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -7,27 +10,150 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    private StackPane rootStackPane;
+    @FXML private StackPane rootStackPane;
 
-    @FXML
-    private JFXButton buttonHelp;
+    @FXML private JFXButton buttonHelp;
+
+    @FXML private Pane boardPane;
+
+    @FXML private Pane row_a;
+    @FXML private Pane row_b;
+    @FXML private Pane row_c;
+    @FXML private Pane row_d;
+    @FXML private Pane row_e;
+    @FXML private Pane row_f;
+    @FXML private Pane row_g;
+    @FXML private Pane row_h;
+
+    private final List<Pane> panes = new ArrayList<>();
+
+    private Point2D offset;
 
     /**
      * Called after window has finished loading.
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        panes.add(row_a);
+        panes.add(row_b);
+        panes.add(row_c);
+        panes.add(row_d);
+        panes.add(row_e);
+        panes.add(row_f);
+        panes.add(row_g);
+        panes.add(row_h);
+    }
 
+    @FXML
+    public void startMovePiece(MouseEvent mouseEvent) {
+        Circle circle = (Circle)mouseEvent.getSource();
+        circle.setOpacity(0.4d);
+        offset = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+
+        circle.setOpacity(1.0d);
+
+        Point2D mousePoint = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+        Point2D mousePoint_s = new Point2D(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+
+        if( !inBoard(mousePoint_s) ) {
+            return;  // don't relocate() b/c will resize Pane
+        }
+
+        Point2D mousePoint_p = circle.localToParent(mousePoint);
+        circle.relocate(mousePoint_p.getX()-offset.getX(), mousePoint_p.getY()-offset.getY());
+    }
+
+    /**
+     * Checks whether the given point is within the bounds of the board's base pane
+     * @param pt The point to check
+     * @return True if within bounds, else false
+     */
+    private boolean inBoard(Point2D pt) {
+        Point2D panePt = boardPane.sceneToLocal(pt);
+        return panePt.getX()-offset.getX() >= 0.0d
+                && panePt.getY()-offset.getY() >= 0.0d
+                && panePt.getX() <= boardPane.getWidth()
+                && panePt.getY() <= boardPane.getHeight();
+    }
+
+    public void finishMovingPiece(MouseEvent mouseEvent) {
+        Circle circle = (Circle)mouseEvent.getSource();
+
+        offset = new Point2D(0.0d, 0.0d);
+
+        Point2D mousePoint = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+        Point2D mousePointScene = circle.localToScene(mousePoint);
+
+        Rectangle r = pickRectangle( mousePointScene.getX(), mousePointScene.getY() );
+
+        final Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        timeline.setAutoReverse(false);
+
+        if( r != null ) {
+
+            Point2D rectScene =r.localToScene(r.getX(), r.getY());
+            Point2D parent = boardPane.sceneToLocal(rectScene.getX(), rectScene.getY());
+
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.millis(100),
+                            new KeyValue(circle.layoutXProperty(), parent.getX()),
+                            new KeyValue(circle.layoutYProperty(), parent.getY()),
+                            new KeyValue(circle.opacityProperty(), 1.0d)
+                    )
+            );
+        } else {
+
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.millis(100),
+                            new KeyValue(circle.opacityProperty(), 1.0d)
+                    )
+            );
+        }
+
+        timeline.play();
+    }
+
+    private Rectangle pickRectangle(MouseEvent evt) {
+        return pickRectangle(evt.getSceneX(), evt.getSceneY());
+    }
+
+    private Rectangle pickRectangle(double sceneX, double sceneY) {
+        Rectangle pickedRectangle = null;
+        for( Pane row : panes ) {
+            Point2D mousePoint = new Point2D(sceneX, sceneY);
+            Point2D mpLocal = row.sceneToLocal(mousePoint);
+            if( row.contains(mpLocal) ) {
+                for( Node cell : row.getChildrenUnmodifiable() ) {
+                    Point2D mpLocalCell = cell.sceneToLocal(mousePoint);
+
+                    if( cell.contains(mpLocalCell) ) {
+                        pickedRectangle = (Rectangle)cell;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        return pickedRectangle;
     }
 
     @FXML
