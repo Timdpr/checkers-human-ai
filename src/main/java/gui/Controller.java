eb84bb341ffd8ba659fe3c02019b509ee57856d9
Timdpr <QOTimSA@live.co.uk>
2018-04-16 13:00:33 +0100

    Add help dialog and wip board

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,4 +1,48 @@
 package main.java.gui;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private StackPane rootStackPane;
+
+    @FXML
+    private JFXButton buttonHelp;
+
+    /**
+     * Called after window has finished loading.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void displayHelp(ActionEvent actionEvent) {
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        dialogLayout.setHeading(new Text("Help"));
+        dialogLayout.setBody(new Text("Some help text, you dummy!"));
+
+        JFXDialog dialog = new JFXDialog(rootStackPane, dialogLayout, JFXDialog.DialogTransition.CENTER);
+
+        JFXButton buttonExit = new JFXButton("Okay");
+        buttonExit.setButtonType(JFXButton.ButtonType.RAISED);
+        buttonExit.setOnAction(event -> dialog.close());
+
+        dialogLayout.setActions(buttonExit);
+        dialog.show();
+    }
 }
