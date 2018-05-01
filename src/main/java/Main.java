a57e1140cefe02be163884990baceba73db28ea0
Timdpr <QOTimSA@live.co.uk>
2018-05-01 22:17:39 +0100

    Add checkbox tooltip and window icons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -15,6 +16,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../res/sample.fxml"));
         primaryStage.setTitle("Minimax Checkers");
+
+        primaryStage.getIcons().addAll(
+                // JavaFX is bad at auto choosing icons, so, many are given:
+                new Image(getClass().getResourceAsStream("../res/icon_16.png")),
+                new Image(getClass().getResourceAsStream("../res/icon_32.png")),
+                new Image(getClass().getResourceAsStream("../res/icon_48.png")),
+                new Image(getClass().getResourceAsStream("../res/icon_64.png")),
+                new Image(getClass().getResourceAsStream("../res/icon_128.png")));
+
         primaryStage.setScene(new Scene(root, 800, 480));
         primaryStage.show();
     }
