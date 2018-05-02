6804b35e3f7fa1528376d7dc95dff4e8ea3045b6
Timdpr <QOTimSA@live.co.uk>
2018-05-02 13:57:07 +0100

    Finish Piece docs

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -1,6 +1,9 @@
 package main.java.model;
 
 /**
+ * A Piece object used in the model board. Simply has a colour, as a 'r' or 'w' char, and a boolean to determine
+ * whether it is a King or not.
+ *
  * @author tp275
  */
 public class Piece {
@@ -9,8 +12,8 @@ public class Piece {
     private boolean isKing;
 
     /**
-     *
-     * @param colour
+     * Creates a new piece with given colour. isKing = false by default.
+     * @param colour the colour to set the piece as
      */
     public Piece(char colour) {
         this.colour = colour;
@@ -18,9 +21,9 @@ public class Piece {
     }
 
     /**
-     *
-     * @param colour
-     * @param isKing
+     * Creates a new piece with given colour and isKing boolean
+     * @param colour the colour to set the piece as
+     * @param isKing sets whether the Piece is a king or not
      */
     public Piece(char colour, boolean isKing) {
         this.colour = colour;
@@ -28,24 +31,24 @@ public class Piece {
     }
 
     /**
-     *
-     * @return
+     * Returns the piece's colour
+     * @return the piece's colour as a char
      */
     public char getColour() {
         return colour;
     }
 
     /**
-     *
-     * @return
+     * Returns whether the piece is a king
+     * @return true if king, else false
      */
     public boolean isKing() {
         return isKing;
     }
 
     /**
-     *
-     * @param isKing
+     * Sets the status of isKing
+     * @param isKing true to set piece as a king, and vice-versa
      */
     public void setKing(boolean isKing) {
         this.isKing = isKing;
