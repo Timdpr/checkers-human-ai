66214bff59b57a41e0b8c07ff85f4c7e1899e10a
Timdpr <QOTimSA@live.co.uk>
2018-05-02 13:57:37 +0100

    Fix file loading from jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 package main.java;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,16 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../res/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/main/res/sample.fxml"));
         primaryStage.setTitle("Minimax Checkers");
 
         primaryStage.getIcons().addAll(
                 // JavaFX is bad at auto choosing icons, so, many are given:
-                new Image(getClass().getResourceAsStream("../res/icon_16.png")),
-                new Image(getClass().getResourceAsStream("../res/icon_32.png")),
-                new Image(getClass().getResourceAsStream("../res/icon_48.png")),
-                new Image(getClass().getResourceAsStream("../res/icon_64.png")),
-                new Image(getClass().getResourceAsStream("../res/icon_128.png")));
+                new Image(getClass().getResourceAsStream("/main/res/icon_16.png")),
+                new Image(getClass().getResourceAsStream("/main/res/icon_32.png")),
+                new Image(getClass().getResourceAsStream("/main/res/icon_48.png")),
+                new Image(getClass().getResourceAsStream("/main/res/icon_64.png")),
+                new Image(getClass().getResourceAsStream("/main/res/icon_128.png")));
 
         primaryStage.setScene(new Scene(root, 800, 480));
         primaryStage.show();
